8e9993f97b84c9b1911b06a4023df4b8c0db9f59
Alin Panaitiu <alin.p32@gmail.com>
2017-11-20 22:14:01 +0200

    Add battery percentage and python env

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,8 +2,19 @@ set -g CMD_DURATION 0
 
 function flash_fst; set_color -o fa0; end
 function flash_snd; set_color -o C00; end
-function flash_trd; set_color -o 666; end
-function flash_dim; set_color -o 333; end
+function flash_trd; set_color -o 777; end
+function flash_dim; set_color -o 444; end
+function flash_env; set_color -o 56C; end
+
+function flash_charging; set_color -o 769; end
+function flash_charging_dim; set_color -o 667; end
+function flash_charged; set_color -o 5C6; end
+function flash_charged_dim; set_color -o 2a5; end
+function flash_midcharged; set_color -o fa0; end
+function flash_midcharged_dim; set_color -o e70; end
+function flash_discharged; set_color -o C00; end
+function flash_discharged_dim; set_color -o B11; end
+
 function flash_off; set_color normal; end
 function bc; command bc -l $argv; end
 
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -11,6 +11,44 @@ function flash_git_is_touched
   test -n (echo (command git status --porcelain))
 end
 
+function flash_venv
+  command pyenv version-name ^/dev/null; or echo ""
+end
+
+function flash_battery_charge
+  if [ (uname) != Darwin ]
+    return 0
+  end
+  set battery_info (pmset -g batt)
+
+  set charging false
+  if string match "*AC Power*" $battery_info >/dev/null
+    set charging true
+  end
+
+  set battcolor "flash_discharged"
+
+  set percentage (echo $battery_info | awk 'match($0, "[0-9]+%;") { print substr($0,RSTART,RLENGTH-2) }')
+  set remaining (echo $battery_info | awk 'match($0, "[0-9]+:[0-9]+ remaining") { print substr($0,RSTART,RLENGTH-10) }')
+
+  if string match -r "[0-9]{1,2}:[0-9]{1,2}" $remaining >/dev/null ^/dev/null
+    set remaining "($remaining)"
+  end
+
+  if [ $charging = true ]
+    set battcolor "flash_charging"
+  else if [ $percentage -gt 70 ]
+    set battcolor "flash_charged"
+  else if [ $percentage -gt 30 ]
+    set battcolor "flash_midcharged"
+  end
+
+  set timecolor $battcolor'_dim'
+
+  printf  " "(eval $battcolor)"$percentage%%"(eval $timecolor)"$remaining"(flash_off)
+end
+
+
 function fish_right_prompt
   set -l code $status
 
@@ -18,22 +56,26 @@ function fish_right_prompt
     test $code -ne 0; and echo (flash_snd); or echo (flash_fst)
   end
 
+  flash_battery_charge
+
   if test $CMD_DURATION -gt 1000
     printf (flash_dim)" ~"(printf "%.1fs " (math "$CMD_DURATION / 1000"))(flash_off)
   end
+
+  printf " "(flash_env)(flash_venv)(flash_off)
 
   if test -d .git
     if flash_git_is_stashed
       echo (flash_dim)"<"(flash_off)
     end
-    printf (begin
+    printf " "(begin
       flash_git_is_touched
         and echo (flash_fst)"(*"(flash_snd)(flash_git_branch_name)(flash_fst)")"(flash_off)
         or echo (flash_snd)"("(flash_fst)(flash_git_branch_name)(flash_snd)")"(flash_off)
     end)(flash_off)
   end
 
-  printf " "(flash_trd)(date +%H(status::color):(flash_dim)%M(status::color):(flash_trd)%S)(flash_snd)" "(flash_off)
+  printf " "(flash_trd)(date +%H(status::color):(flash_trd)%M)(flash_snd)" "(flash_off)
 
   if test $code -ne 0
     echo (flash_fst)"≡ "(flash_snd)"$code"(flash_off)
