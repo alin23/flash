c9ada65ba9d55251dba8a122c73c2b7c1e5f593a
Alin Panaitiu <alin.p32@gmail.com>
2018-04-19 20:44:45 +0300

    Fix format path

--- fish_title.fish
+++ fish_title.fish
@@ -6,6 +6,11 @@ function ssh_hostname_simple
   end
 end
 
+function format_path_simple
+  set -l sepright ' ) '
+  set -l base (basename "$argv[1]")
+  string replace -a '/' $sepright $argv[1] | string replace -r (string escape $base)'$' $base
+end
 
 function fish_prompt_simple
   set -l sepleft '('
@@ -15,11 +20,11 @@ function fish_prompt_simple
 
   printf (begin
     if string match -q '/*' $prompt
-      format_path (string replace '/' "$sepleft # $sepright " $prompt)
+      format_path_simple (string replace '/' "$sepleft # $sepright " $prompt)
     else if string match -q '~*' $prompt
-      format_path (string replace -r '~/?' "$sepleft ⌁ $sepright " $prompt)
+      format_path_simple (string replace -r '~/?' "$sepleft ⌁ $sepright " $prompt)
     else
-      format_path $prompt
+      format_path_simple $prompt
     end
   end)(test (string length $prompt) -ne 1; and echo " $sepright "; or echo '')
 end
