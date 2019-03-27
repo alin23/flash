77d650961af994020d0ce66cc2c9ea31ee024810
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-03-27 16:42:03 +0200

    Simplify title

--- fish_title.fish
+++ fish_title.fish
@@ -1,36 +1,16 @@
-function ssh_hostname_simple
-  if set -q SSH_CLIENT
-    printf (hostname)" SSH \u276F "
-  else
-    echo -n ''
-  end
-end
-
-function format_path_simple
-  set -l sepright ' ) '
-  set -l base (basename "$argv[1]")
-  string replace -a '/' $sepright $argv[1] | string replace -r (string escape $base)'$' $base
-end
-
-function fish_prompt_simple
-  set -l sepleft '('
-  set -l sepright ')'
-
-  set -l prompt (prompt_pwd)
-
-  printf (begin
-    if string match -q '/*' $prompt
-      format_path_simple (string replace '/' "$sepleft # $sepright " $prompt)
-    else if string match -q '~*' $prompt
-      format_path_simple (string replace -r '~/?' "$sepleft ⌁ $sepright " $prompt)
-    else
-      format_path_simple $prompt
-    end
-  end)(test (string length $prompt) -ne 1; and echo " $sepright "; or echo '')
-end
-
 function fish_title
-  set -l _ssh_hostname (ssh_hostname_simple)
-  set -l _last_cmd (fish_last_cmd $argv)
-  echo "$_ssh_hostname $_last_cmd"
+	set -q SSH_CLIENT
+	and printf (hostname)" SSH \u276F "
+
+	set -l cmd (printf "%.20s" "$argv")
+
+	if test "$PWD" != "$HOME"
+		if test -n "$cmd"
+			printf (basename $PWD)" \u276F $cmd"
+		else
+			basename $PWD
+		end
+	else
+		echo -n '~'
+	end
 end
