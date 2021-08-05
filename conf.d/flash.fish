469c836977881a231a173449432abd31db3170e9
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-08-05 10:09:19 +0300

    Fix prompt

--- conf.d/flash.fish
+++ conf.d/flash.fish
@@ -2,9 +2,13 @@ if test "$LC_TERMINAL" = iTerm2
   set_terminal_256_colors
 end
 
-if not test -f (dirname (status filename))/kitty.fish
-  set KITTY_INTEGRATION_SCRIPT (kitty +runpy "from kitty.constants import *; print(shell_integration_dir)")/kitty.fish
+if status --is-interactive; and not test -f (dirname (status filename))/kitty.fish
+  set KITTY_INTEGRATION_SCRIPT ""
+  if which kitty 2>/dev/null >/dev/null
+    set KITTY_INTEGRATION_SCRIPT (kitty +runpy "from kitty.constants import *; print(shell_integration_dir)")/kitty.fish
+  end
   export KITTY_SHELL_INTEGRATION="enabled"
+
   if test -f "$KITTY_INTEGRATION_SCRIPT"
     source "$KITTY_INTEGRATION_SCRIPT"
   else
