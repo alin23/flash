0a041f176a35b0a46032bc0a23d2cd1006b4c392
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-08-06 16:40:16 +0300

    fix

--- conf.d/flash.fish
+++ conf.d/flash.fish
@@ -4,8 +4,9 @@ end
 
 if status --is-interactive; and not test -f (dirname (status filename))/kitty.fish
   set KITTY_INTEGRATION_SCRIPT ""
-  if which kitty 2>/dev/null >/dev/null
-    set KITTY_INTEGRATION_SCRIPT (kitty +runpy "from kitty.constants import *; print(shell_integration_dir)")/kitty.fish
+  set KITTY (which kitty 2>/dev/null)
+  if test -n "$KITTY"
+    set KITTY_INTEGRATION_SCRIPT ($KITTY +runpy "from kitty.constants import *; print(shell_integration_dir)")/kitty.fish
   end
   export KITTY_SHELL_INTEGRATION="enabled"
 
